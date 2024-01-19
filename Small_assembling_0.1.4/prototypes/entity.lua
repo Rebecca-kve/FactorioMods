b81f48f90e9e5e4bd692a733d05ae84dad6eaa67
Rebecca-kve <55534811+Rebecca-kve@users.noreply.github.com>
2024-01-19 14:51:54 +0100

    ingredients_setting

--- Small_assembling_0.1.4/prototypes/entity.lua
+++ Small_assembling_0.1.4/prototypes/entity.lua
@@ -1,5 +1,6 @@
 speed_multiplier = settings.startup["small-assembling-speed"].value
 consumption_multiplier = settings.startup["consumption_multiplier"].value
+ngredients_setting = settings.startup["small-assembling-ingredients"].value
 
 local small_assembling_machine_1_item = util.table.deepcopy(data.raw["item"]["assembling-machine-1"])
 small_assembling_machine_1_item.order = "[1]small-assembling-machine"
@@ -46,7 +47,7 @@ data:extend({small_assembling_machine_1_item, small_assembling_machine_2_item, s
 local small_assembling_machine_1 = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 small_assembling_machine_1.name = "small-assembling-machine-1"
 small_assembling_machine_1.minable.result = "small-assembling-machine-1"
-small_assembling_machine_1.ingredient_count = 1
+small_assembling_machine_1.ingredient_count = ingredients_setting
 small_assembling_machine_1.crafting_speed = 0.5 * speed_multiplier
 --small_assembling_machine_1.module_specification = { module_slots = 2}
 --small_assembling_machine_1.allowed_effects = {"consumption", "speed", "productivity", "pollution"}
@@ -109,7 +110,7 @@ small_assembling_machine_1.animation = {
 local small_assembling_machine_2 = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 small_assembling_machine_2.name = "small-assembling-machine-2"
 small_assembling_machine_2.minable.result = "small-assembling-machine-2"
-small_assembling_machine_2.ingredient_count = 1
+small_assembling_machine_2.ingredient_count = ingredients_setting
 small_assembling_machine_2.crafting_speed = 0.75 * speed_multiplier
 small_assembling_machine_2.module_specification = { module_slots = 2}
 small_assembling_machine_2.allowed_effects = {"consumption", "speed", "productivity", "pollution"}
@@ -170,7 +171,7 @@ small_assembling_machine_2.animation = {
 local small_assembling_machine_3 = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 small_assembling_machine_3.name = "small-assembling-machine-3"
 small_assembling_machine_3.minable.result = "small-assembling-machine-3"
-small_assembling_machine_3.ingredient_count = 1
+small_assembling_machine_3.ingredient_count = ingredients_setting
 small_assembling_machine_3.crafting_speed = 1.25 * speed_multiplier
 small_assembling_machine_3.module_specification = { module_slots = 4}
 small_assembling_machine_3.allowed_effects = {"consumption", "speed", "productivity", "pollution"}

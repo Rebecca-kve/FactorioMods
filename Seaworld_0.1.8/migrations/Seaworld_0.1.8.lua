26577697476bf6444f3d4d2cff200091dffb4b1b
Rebecca-kve <55534811+Rebecca-kve@users.noreply.github.com>
2024-02-22 16:30:41 +0100

    migration

--- Seaworld_0.1.8/migrations/Seaworld_0.1.8.lua
+++ Seaworld_0.1.8/migrations/Seaworld_0.1.8.lua
@@ -14,17 +14,17 @@ local oldEntityName = "SW-burner-beacon"
 local newEntityName = "SW-burner-furnace"
 
 -- Define the function to perform the migration
-local function performMigration()
+local function migrate_to_version_0_1_8()
 
 
     for _, surface in pairs(game.surfaces) do
         local entities = surface.find_entities_filtered({name = oldEntityName})
         for _, entity in pairs(entities) do
             -- Preserve items from burnt inventory
-            local burntInventory = entity.burner and entity.burner.inventory
+            local burntInventory = entity.burner and entity.burner.burnt_result_inventory 
             if burntInventory then
                 for item, count in pairs(burntInventory.get_contents()) do
-                    entity.burner.inventory.remove({name = item, count = count})
+                    entity.burner.burnt_result_inventory .remove({name = item, count = count})
                     game.players[1].insert({name = item, count = count})
                 end
             end
@@ -46,7 +46,7 @@ local function performMigration()
             local inventory = chest.get_inventory(defines.inventory.chest)
             for name, count in pairs(inventory.get_contents()) do
                 if name == oldEntityName then
-                    inventory.remove{name = oldEntityName}
+                    inventory.remove{name = oldEntityName, count = count}
                     inventory.insert{name = newEntityName, count = count}
                 end
             end
@@ -58,16 +58,22 @@ local function performMigration()
         local inventory = player.get_main_inventory()
         for name, count in pairs(inventory.get_contents()) do
             if name == oldEntityName then
-                inventory.remove{name = oldEntityName}
+                inventory.remove{name = oldEntityName, count = count}
                 inventory.insert{name = newEntityName, count = count}
             end
         end
     end
+
+
 end
 
 -- Register the migration script
-script.on_configuration_changed(function(data)
-    if data.mod_changes and data.mod_changes["Seaworld"] then
-        performMigration()
+
+script.on_configuration_changed(function(event)
+    if event.mod_changes["Seaworld"] and event.mod_changes["Seaworld"].old_version then
+        local old_version = event.mod_changes["Seaworld"].old_version
+        if old_version < "0.1.8" then
+            migrate_to_version_0_1_8()
+        end
     end
 end)

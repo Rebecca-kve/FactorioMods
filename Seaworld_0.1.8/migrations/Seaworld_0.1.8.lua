9b601f5220aa09ca00f61b0c4cb3dd2f3252c27d
Rebecca-kve <55534811+Rebecca-kve@users.noreply.github.com>
2024-02-26 16:22:48 +0100

    .8

--- Seaworld_0.1.8/migrations/Seaworld_0.1.8.lua
+++ Seaworld_0.1.8/migrations/Seaworld_0.1.8.lua
@@ -4,6 +4,9 @@ local newEntityName = "SW-burner-furnace"
 
 local function migrate_to_version_0_1_8()
 
+    -- Keep track of whether items have been distributed
+    local itemsDistributed = false
+
     for _, surface in pairs(game.surfaces) do
         local entities = surface.find_entities_filtered({name = oldEntityName})
         for _, entity in pairs(entities) do
@@ -12,38 +15,8 @@ local function migrate_to_version_0_1_8()
             if burntInventory then
                 for item, count in pairs(burntInventory.get_contents()) do
                     entity.burner.burnt_result_inventory .remove({name = item, count = count})
-                    game.players[1].insert({name = item, count = count})
-                end
-            end
-
-            -- Replace the entity with the new furnace
-            local newEntity = surface.create_entity({
-                name = newEntityName,
-                position = entity.position,
-                force = entity.force
-            })
-            entity.destroy()
-        end
-    end
-end
-
---[[
-local function migrate_to_version_0_1_8()
-    -- Keep track of whether items have been distributed
-    local itemsDistributed = false
-    
-    -- Iterate over all surfaces
-    for _, surface in pairs(game.surfaces) do
-        -- Find all entities of the old entity type
-        local entities = surface.find_entities_filtered({name = oldEntityName})
-        for _, entity in pairs(entities) do
-            -- Preserve items from burnt inventory
-            local burntInventory = entity.burner and entity.burner.burnt_result_inventory 
-            if burntInventory then
-                for item, count in pairs(burntInventory.get_contents()) do
-                    burntInventory.remove({name = item, count = count})
-                    -- Insert items into the player's inventory
-                    if not itemsDistributed then
+					
+					if not itemsDistributed then
                         game.players[1].insert({name = item, count = count})
                         itemsDistributed = true
                     end
@@ -60,8 +33,6 @@ local function migrate_to_version_0_1_8()
         end
     end
 end
-
-]]
 
 -- Register the migration script
 

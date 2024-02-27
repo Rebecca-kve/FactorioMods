be0f1a149d4d0d7be98ce55c4326530ef61b2d61
Rebecca-kve <55534811+Rebecca-kve@users.noreply.github.com>
2024-02-27 08:03:11 +0100

    1.9

--- Seaworld_0.1.9/migrations/Seaworld_0.1.7.lua
+++ Seaworld_0.1.9/migrations/Seaworld_0.1.7.lua
@@ -2,7 +2,7 @@ for _, force in pairs(game.forces) do
 	if force.technologies['sulfur-processing'].researched then
 		force.recipes["dirty-water-filtration-uranium-ore"].enabled = true
 	end
-	if force.technologies['kr-quarry-minerals-extraction'].researched and not mods["space-exploration"] then
+	if force.technologies['kr-quarry-minerals-extraction'].researched and not script.active_mods["space-exploration"] then
 		force.recipes["dirty-water-filtration-raw-imersite"].enabled = true
 	end
 end
